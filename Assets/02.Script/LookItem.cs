16410683e1f3ca8e03659e39508e7fe176d9acbe
gahyun3892 <66283620+gahyun3892@users.noreply.github.com>
2020-10-21 11:27:32 +0900

    20201021

---
+++ Assets/02.Script/LookItem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class LookItem : MonoBehaviour
+{
+
+    public void OnLookItemBox(bool isLookAt)
+        {
+            Debug.Log(isLookAt);
+        MoveCtrl.isStopped = isLookAt;
+        }
+
+    public void OnPosterEnter(PointerEventData eventData)
+    {
+        MoveCtrl.isStopped = true;
+    }
+
+    public void OnPosterExit(PointerEventData eventData)
+    {
+        MoveCtrl.isStopped = false;
+    }
+
+    public void OnGvrPointerHover(PointerEventData eventData)
+    {
+        Debug.Log("Reticle On");
+    }
+}
